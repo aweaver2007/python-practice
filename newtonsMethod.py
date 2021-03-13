3fa6743e0b5234fc29dac2a7d7da024f6dd4c595
Alexander Weaver <aweaver@davidsonacademy.unr.edu>
2021-03-12 23:57:57 -0800

    added main function and started creating classes for nodes

--- newtonsMethod.py
+++ newtonsMethod.py
@@ -1,9 +1,20 @@
 """
-This is a program that will use newtons method to find the deri
+This is a program that will find the derivatives of functions and use that 
+with Newton's method to find the roots of functions.
 Author: Alexander Weaver
-Date: 11/5/2020
+Date: 3/12/2021
 Version: Python 3.6.5
 """
-functionString=input("f(x)=")
-print("Hello world")
-print("is this working?")
+
+def main():
+    functionString=input("f(x)=")
+ 
+#creating classes for operators
+class node(object):
+    pass
+
+class addOp(node):
+    def __init__(self, left, right)
+
+if __name__=="__main__":
+    main()
